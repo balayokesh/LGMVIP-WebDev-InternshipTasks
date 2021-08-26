043e0494455acd2a5fa4aea943dc4e02c8a18466
balayokesh <balayokeshmani@gmail.com>
2021-08-27 01:20:57 +0530

    completed UI design for task 2

--- task2/src/App.js
+++ task2/src/App.js
@@ -1,40 +1,75 @@
 import React, { useState, useEffect } from 'react';
+import { Container, AppBar, Card, CardMedia, CardContent, Typography, makeStyles, Button, CircularProgress } from '@material-ui/core';
 
 function App() {
 	const [users, setUsers] = useState([])
+	const [loader, setLoader] = useState(false);;
 
-	useEffect(() => {
+	const loadUsers = () => {
+		setLoader(true);
 		(async () => {
 			const response = await fetch("https://reqres.in/api/users?page=1");
 			const JSONres = await response.json();
 			setUsers(JSONres.data);
+			console.log(users);
+			setLoader(false);
 		})()
-	}, []);
-	console.log(users);
+	}
+
+	const useStyles = makeStyles ({
+		image: {
+			height: "300px",
+			minWidth: "400px",
+		},
+		flex: {
+			display: "grid",
+			gridTemplateColumns: "auto auto auto"
+		},
+		padding: {
+			padding: '2%',
+		}
+	});
+	const classes = useStyles ();
 
 	return (
-		<div>
-			<header>
-				XYZ company
-			</header>
-			<main>
-				<h1>Hello!</h1>
-				<div>
+		<>
+			<AppBar position='static'>
+				<Typography variant='h4' align='center' className={classes.padding}>XYZ company</Typography>
+			</AppBar>
+			
+			<center className={classes.padding}>
+				<Typography variant='h5'>Welcome!</Typography>
+				<br />
+				<Button variant='contained' color='primary' onClick={loadUsers}>Load users</Button>
+				<br />
+				<br />
+				{
+				loader === true ? <CircularProgress /> : console.log("loader false")
+				}
+			</center>
+			<Container>
+				<div className={classes.flex}>
 					{
 					users.map ((user) => (
-						<div key={user.id}>
-							<p>Name: {user.first_name + " " + user.last_name}</p>
-							<p>Email: {user.email}</p>
-							<img src={user.avatar} />
-						</div>
+						<Card key={user.id}>
+
+							<CardMedia
+								className={classes.image}
+								image = {user.avatar} 
+								title = {user.first_name}
+							/>
+
+							<CardContent>
+								<Typography>Name: {user.first_name + " " + user.last_name}</Typography>
+								<Typography>Email: {user.email}</Typography>
+							</CardContent>
+
+						</Card>
 					))
 					}
 				</div>
-			</main>
-			<footer>
-				&copy; XYZ company 2021
-			</footer>
-		</div>
+			</Container>
+		</>
 	);
 }
 
